2ca1beab49b9263a2c0aecdade3016c97201bbc4
depave <0709kjm@gmail.com>
2021-11-02 18:20:27 +0900

    re-first commit

---
+++ sample.py
@@ -0,0 +1,13 @@
+'''
+for i in range(5):
+	for j in range(i+1):
+		print('*', end="")
+	print('')	
+'''
+
+<<<<<<< HEAD
+#test
+=======
+print("This is modified file")
+>>>>>>> parent of 2594b5f... Revert "second commit"
+
